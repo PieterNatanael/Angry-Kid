da1459304cb394b625ca4286cb5bfc99b2639eec
PieterNatanael <pieternatanael@icloud.com>
2024-04-27 10:33:15 +0700

    Initial Commit

---
+++ Angry Kid/Angry_KidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Angry_KidApp.swift
+//  Angry Kid
+//
+//  Created by Pieter Yoshua Natanael on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Angry_KidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Angry Kid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Angry Kid
+//
+//  Created by Pieter Yoshua Natanael on 27/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
